93b300ae5b525f1beda844cf925d2770f6e7c211
unknown <o.sabodash@mail.ru>
2018-09-19 21:22:44 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,18 +1,105 @@
+
+
+
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = () => { };
+const createNotEnumerableProperty = (propertyName) => {
+    Object.defineProperty(Object.prototype, propertyName, {
+        get: function () { return Object.prototype._value },
+        set: function (value) { return Object.prototype._value = value }
+    });
+    return propertyName;
+};
+const createProtoMagicObject = () => {
+    var magObj = function () { return {} };
+    magObj.prototype = magObj.__proto__;
+    return magObj;
+};
+
+var numbCallFunction = 0;
+Function.prototype.valueOf = function () { return numbCallFunction; }
+const incrementor = () => {
+    numbCallFunction++;
+    return incrementor;
+};
+
+var numbCallAsyncFunction = 0;
+const asyncIncrementor = () => {
+    var number = new Promise(function (resolve) {
+        numbCallAsyncFunction++;
+        return resolve(numbCallAsyncFunction);
+    });
+
+    return number;
+
+
+};
+
+
+const createIncrementer = () => {
+    var inc = {
+        count: 0,
+        next() {
+            return { value: ++this.count }
+        },
+        
+        [Symbol.iterator]() {
+            return {
+                next: () => {
+                    if (this.count > 10) {
+                        return this;
+                    }
+
+                    return this.next();
+
+
+                },
+            };
+        },
+    };
+    return inc;
+
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = (param) => {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            return resolve(param);
+        }, 1005);
+    });
+};
+
+
+var countLenght = 0;
+function isEmpty(params) {
+    for (var key in params) {
+        countLenght++;
+      }
+      return countLenght;
+}
+const getDeepPropertiesCount = (obj) => {
+
+    let arr = Object.getOwnPropertyNames(obj);
+    var a = arr.length;
+    arr.forEach(function(i) {
+        if (isEmpty(obj[i]) > 0){
+            a += getDeepPropertiesCount(obj[i]);
+        };
+    });
+    return a;
+        
+    
+};
+const createSerializedObject = () => { 
+    return null;
+};
+const toBuffer = () => { };
+const sortByProto = (arr) => {
+
+return arr
+ };
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
